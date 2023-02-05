68429f4b49d6d743fcf914e699b13f2e135583a2
José Manuel Díaz Urraco <48102058+jmdu99@users.noreply.github.com>
2023-02-05 19:50:33 +0100

    Update run_summarization_no_trainer.py

--- run_summarization_no_trainer.py
+++ run_summarization_no_trainer.py
@@ -142,7 +142,6 @@ teacher_with_24_layers_distill_mappings = {
                 24: {0: 0, 1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9, 10: 10, 11: 11, 12: 12, 13: 13, 14: 14, 15: 15, 16: 16, 17: 17, 18: 18, 19: 19, 20: 20, 21: 21, 22: 22, 23: 23}
 }
 
-
 NUMS = [str(i) for i in range(24)]
 
 
@@ -516,6 +515,14 @@ def main():
 
         dst_dict = student_model.state_dict()  # Initilized student model state dict, needs loading weights
         src_dict = teacher_model.state_dict()  # Pretrained teacher model state dict, whose weights will be loaded
+
+        print('Teacher model state dict items:')
+        for key, value in src_dict.items():
+            print(key, value)
+
+        print('Student model state dict items:')
+        for key, value in dst_dict.items():
+            print(key, value)
 
         for key in dst_dict.keys():
             if ("encoder" in key or "decoder" in key) and key[
